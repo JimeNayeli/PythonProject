df059c66368efb31b661a3bb1f79ecd936c319f8
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2023-04-20 21:20:12 -0500

    Add files via upload

---
+++ PythonG3Final/Obstaculo.py
@@ -0,0 +1,35 @@
+import pygame
+from random import randint, choice
+
+class Obstacle(pygame.sprite.Sprite):
+	def __init__(self,type):
+		super().__init__()
+		
+		if type == 'fly':
+			fly_1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+			fly_2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+			self.frames = [fly_1,fly_2]
+			y_pos = 210
+		else:
+			crocodile_1 = pygame.image.load('graphics/crocodile/crocodile1.png').convert_alpha()
+			crocodile_2 = pygame.image.load('graphics/crocodile/crocodile2.png').convert_alpha()
+			self.frames = [crocodile_1,crocodile_2]
+			y_pos  = 300
+
+		self.animation_index = 0
+		self.image = self.frames[self.animation_index]
+		self.rect = self.image.get_rect(midbottom = (randint(900,1100),y_pos))
+
+	def animation_state(self):
+		self.animation_index += 0.1 
+		if self.animation_index >= len(self.frames): self.animation_index = 0
+		self.image = self.frames[int(self.animation_index)]
+
+	def update(self):
+		self.animation_state()
+		self.rect.x -= 6
+		self.destroy()
+
+	def destroy(self):
+		if self.rect.x <= -100: 
+			self.kill()
